d575a112e0b8117e1e15b4d66ff0b5a7f4934aba
novotny <novotny@ufl.edu>
2017-01-25 17:43:14 -0700

    adding config file

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+    // uri: 'mongodb://steven:password1@ds037234.mlab.com:37234/cen3031bootcamp4'
+    uri: 'mongodb://steven:password1@ds117829.mlab.com:17829/cen3031bootcamp3'
+  },
+  googleMaps: {
+    key: ''
+  },
+  port: 8080
+};
